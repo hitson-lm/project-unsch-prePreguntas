b4ff89b41429fcbb292fba573796cbc416e8b7a4
hitson-lm <hitson.lm@gmail.com>
2021-12-15 23:48:49 -0500

    projecto main

---
+++ src/main/java/com/unsch/prepreguntas/PrePreguntasApplication.java
@@ -0,0 +1,13 @@
+package com.unsch.prepreguntas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrePreguntasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrePreguntasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unsch/prepreguntas/PrePreguntasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unsch.prepreguntas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrePreguntasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
